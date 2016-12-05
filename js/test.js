38aeb8fe9e295a5c91fc51de0f6f0eed2bfcce3a
Mitchell Waldman <mitchswaldman@gmail.com>
2016-12-04 23:51:05 -0800

    Update phrases.

--- js/test.js
+++ js/test.js
@@ -38,7 +38,8 @@ $(function(){
 					"But wherever you are at this moment,",
 					"I hope this message finds you happy and well,",
 					"And I hope life has been as good to you as you are to it.",
-					"I will always love you. I'm sorry that I didn't show that all the time."
+					"I will always love you.", 
+					"I'm sorry that I didn't show that all the time.",
 					"Now back to our regular scheduled programming..."];
 
 	var memories = ["First Date",
